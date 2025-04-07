680bda6e5bd0c5f8213786da44d1d2ddab46dfc4
lvallet <vallet.leo@protonmail.com>
2025-04-07 12:16:42 +0200

    init

---
+++ app/src/main/java/com/fioremini/app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.fioremini.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        
+        // Enable JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
+        // Prevent links from opening in external browser
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load the local HTML file
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    // Handle back button presses within the WebView
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
